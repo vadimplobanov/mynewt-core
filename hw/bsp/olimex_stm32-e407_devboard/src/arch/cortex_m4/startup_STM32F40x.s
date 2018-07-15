f0b23eabda9e14a90aa2f78830f1f1e45226bfa5
Vadim Lobanov <vadimplobanov@gmail.com>
2018-07-15 01:38:20 -0700

    Remove unused stack area, to free up more memory.

--- hw/bsp/olimex_stm32-e407_devboard/src/arch/cortex_m4/startup_STM32F40x.s
+++ hw/bsp/olimex_stm32-e407_devboard/src/arch/cortex_m4/startup_STM32F40x.s
@@ -37,7 +37,7 @@
 #ifdef __STACK_SIZE
     .equ    Stack_Size, __STACK_SIZE
 #else
-    .equ    Stack_Size, 0xc00
+    .equ    Stack_Size, 0x0
 #endif
     .globl    __StackTop
     .globl    __StackLimit
