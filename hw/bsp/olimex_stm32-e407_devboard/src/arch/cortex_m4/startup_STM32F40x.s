8c834d9366967fee7c5ea93280f28cc5baafe5ef
Vadim Lobanov <vadimplobanov@gmail.com>
2018-07-15 21:22:54 -0700

    Revert "Remove unused stack area, to free up more memory."
    Actually, this is the interrupt stack, so it's important.
    
    This reverts commit f0b23eabda9e14a90aa2f78830f1f1e45226bfa5.

--- hw/bsp/olimex_stm32-e407_devboard/src/arch/cortex_m4/startup_STM32F40x.s
+++ hw/bsp/olimex_stm32-e407_devboard/src/arch/cortex_m4/startup_STM32F40x.s
@@ -37,7 +37,7 @@
 #ifdef __STACK_SIZE
     .equ    Stack_Size, __STACK_SIZE
 #else
-    .equ    Stack_Size, 0x0
+    .equ    Stack_Size, 0xc00
 #endif
     .globl    __StackTop
     .globl    __StackLimit
